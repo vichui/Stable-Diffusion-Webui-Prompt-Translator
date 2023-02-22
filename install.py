ba474691200ae91278062f4eb6faad2bf0b1a52f
vichui <33508263+vichui@users.noreply.github.com>
2023-02-22 23:37:29 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
 import launch
 
 if not launch.is_installed("deep-translator"):
-    launch.run_pip("install deep-translator", "requirement for google free")
+    launch.run_pip("install deep-translator", "requirement for deep-translator")
